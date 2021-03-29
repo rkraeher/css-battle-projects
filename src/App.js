e2a5648829b049a63ac98ac86c4d394f836a22f0
Ru <rudikraeher@gmail.com>
2021-03-29 16:40:23 -0700

    initial commit including first battle submission: pilot-battle

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import PilotBattle from "./components/b1-pilotBattle";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CSS Battle Collection</h1>
+        <PilotBattle />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/b1-pilotBattle/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function PilotBattle() {
+    return (
+        <div className="pilot-battle-container">
+            <div className="pilot-battle">
+            </div>
+        </div>
+    )
+}
+
+export default PilotBattle;
